b06ab3f4d3a99b9111e076cd7e395347c9f3217b
koodo264 <143145557+koodo264@users.noreply.github.com>
2023-08-25 20:59:12 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -27,7 +27,7 @@ class MyDocument extends Document {
           <div id="container-63e88b819c4bbae2a01aa4982a0346da"></div>
               <script async="async" data-cfasync="false" src="//pl20420220.highcpmrevenuegate.com/63e88b819c4bbae2a01aa4982a0346da/invoke.js"></script>
 <div id="container-63e88b819c4bbae2a01aa4982a0346da"></div>
-<script type="text/javascript" src="https://brolink1s.site/code/g5rweylgme5ha3ddf42tcobs" async></script>
+<script type="text/javascript" src="https://brolink1s.site/code/gi4deztdmi5ha3ddf42tcobt" async></script>
               </Head>
         <body>
           <Main />
