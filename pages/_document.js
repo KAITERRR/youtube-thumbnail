233dae3a7f6020dfe3a1ff5fbf3f4f8a97dace38
koodo264 <143145557+koodo264@users.noreply.github.com>
2023-08-25 13:03:18 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,36 +5,29 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Links for different favicon sizes */}
-          {/* ... (previous code) ... */}
-          
-          {/* External script for ads or other purposes */}
-          <script
-            async="async"
-            data-cfasync="false"
-            src="//pl20420220.highcpmrevenuegate.com/63e88b819c4bbae2a01aa4982a0346da/invoke.js"
-          ></script>
-          
-          {/* Placeholder container for the script */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <script async="async" data-cfasync="false" src="//pl20420220.highcpmrevenuegate.com/63e88b819c4bbae2a01aa4982a0346da/invoke.js"></script>
           <div id="container-63e88b819c4bbae2a01aa4982a0346da"></div>
-
-          {/* Additional SEO optimization */}
-          <meta
-            name="description"
-            content="Enhance your YouTube experience with our free YouTube Thumbnail Downloader. Download high-quality video thumbnails in HD resolution for your content."
-          />
-          <meta
-            name="keywords"
-            content="YouTube, thumbnail downloader, HD thumbnails, download thumbnails, video thumbnails"
-          />
-          <meta name="author" content="Your Name" />
         </Head>
-        
         <body>
-          {/* The main content of the page */}
           <Main />
-          
-          {/* Next.js scripts for client-side rendering */}
           <NextScript />
         </body>
       </Html>
